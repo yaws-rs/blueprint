2b71e6827f0724f257ef0ccfc2a55968c9c538a3
pinkforest <36498018+pinkforest@users.noreply.github.com>
2025-08-26 21:45:18 +0000

    Add Left Right traits

--- src/traits.rs
+++ src/traits.rs
@@ -1,13 +1,28 @@
 //! Blueprint traits
 
+/// Left side of state machine I/O
+pub trait Left {
+    /// Input of Left side
+    fn bufs(&mut self) -> (&mut [u8], &mut [u8]);
+}
+
+/// Right side of state machine I/O
+pub trait Right {}
+
 /// ..
 pub trait Orbit {
     /// .
     type Position;
     /// .
     type Error;
-    /// .
-    fn advance_with<B>(&mut self, _: &mut B, _: &mut [u8]) -> Result<Self::Position, Self::Error>;
+    /// Userdata, Left In, Left Out, Right In, Right Out
+    //    fn advance_with<B>(&mut self, _: &mut B, _: &mut [u8], _: &mut [u8]) -> Result<Self::Position, Self::Error>;
+    fn advance_with<B, L: Left, R: Right>(
+        &mut self,
+        _: &mut B,
+        _: &mut L,
+        _: &mut R,
+    ) -> Result<Self::Position, Self::Error>;
 }
 
 /// .
@@ -15,7 +30,7 @@ pub trait BluePrint<O: Orbit> {
     /// .
     type Config;
     /// .
-    type Error;    
+    type Error;
     /// .
     fn with_defaults() -> Result<O, Self::Error>;
     /// .
