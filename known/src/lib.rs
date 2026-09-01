6a172f68eac2ca67d85046caf25e28c7446a9721
pinkforest(she/her) <36498018+pinkforest@users.noreply.github.com>
2026-09-01 14:45:09 +1000

    Add yTLS-server as known blueprint (#8)

--- known/src/lib.rs
+++ known/src/lib.rs
@@ -11,9 +11,12 @@ pub use blueprint::{Orbit, Left, Right, NoLeft, NoRight};
 
 /// All the known Orbits enum-dispatch
 pub enum Orbits {
+    /// Known yTls Server Orbit
+    #[cfg(feature = "ytls-server")]
+    YtlsServer(blueprint_ytls::TlsServerOrbit),
     /// Known Tls Orbit
-    #[cfg(feature = "tls")]
-    Tls(blueprint_tls::TlsContext),
+    #[cfg(feature = "rustls")]
+    Rustls(blueprint_rustls::TlsContext),
     /// Known TickTock Orbit
     #[cfg(feature = "tick-tock")]
     TickTock(blueprint_tick_tock::TickTocking),
@@ -37,14 +40,29 @@ pub struct NoPosition;
 #[derive(Debug)]
 pub struct NoError;
 
+#[cfg(feature = "ytls-server")]
+use blueprint_ytls::{TlsServerCtxConfig, CryptoConfig, CryptoRng};
+
+/*
+#[cfg(not(feature = "ytls-server"))]
+mod dummy {
+    struct TlsServerCtxConfig;
+    struct CryptoConfig;
+    struct CryptoRng;
+}
+#[cfg(not(feature = "ytls-server"))]
+use dummy::*; */
+
 impl Orbit for Orbits {
     type Position = NoPosition;
     type Error = NoError;
     #[inline]
     fn advance_with<B, L: Left, R: Right>(&mut self,b: &mut B,l: &mut L,r: &mut R) -> Result<Self::Position, Self::Error> {
         _ = match self {
-            #[cfg(feature = "tls")]
-            Self::Tls(t) => { t.advance_with(b, l, r); },
+            #[cfg(feature = "ytls-server")]
+            Self::YtlsServer(t) => { t.advance_with(b, l, r); },
+            #[cfg(feature = "rustls")]
+            Self::Rustls(t) => { t.advance_with(b, l, r); },
             #[cfg(feature = "tick-tock")]
             Self::TickTock(t) => { t.advance_with(b, l, r); },
             #[cfg(feature = "h11server")]
